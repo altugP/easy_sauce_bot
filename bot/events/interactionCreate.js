4ac32ee2428b4d2e2a97d3f180107f08a5439e79
Altug Paycin <altug.paycin@gmail.com>
2021-09-29 11:13:51 +0200

    Added buttons command to test out button interactions.

--- bot/events/interactionCreate.js
+++ bot/events/interactionCreate.js
@@ -6,6 +6,36 @@ const data = {
     once: false,
 }
 
+async function handleCommand(interaction) {
+    // Fetching the command from the client's `commands` Collection.
+    const command = interaction.client.commands.get(interaction.commandName)
+
+    // If the command doesn't exist, something went wrong.
+    //? Could have been a deleted command.
+    if (!command) return
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+        console.error(error)
+        await interaction.reply({
+            content: 'An error occurred while executing this command.',
+            ephemeral: true, //? Only visible to the user that called this.
+        })
+    }
+}
+
+async function handleButton(interaction) {
+    await interaction.deferReply()
+    if (interaction.customId === 'primary') {
+        await interaction.editReply('Primary')
+    } else if (interaction.customId === 'secondary') {
+        await interaction.editReply('Secondary')
+    } else {
+        await interaction.editReply('Oi.')
+    }
+}
+
 // ############################################################################
 // Exports.
 // ############################################################################
@@ -13,24 +43,10 @@ module.exports = {
     name: data.name,
     once: data.once,
     async execute(interaction) {
-        // If the interaction is not a command it can be ignored for now.
-        if (!interaction.isCommand()) return
-
-        // Fetching the command from the client's `commands` Collection.
-        const command = interaction.client.commands.get(interaction.commandName)
-
-        // If the command doesn't exist, something went wrong.
-        //? Could have been a deleted command.
-        if (!command) return
-
-        try {
-            await command.execute(interaction)
-        } catch (error) {
-            console.error(error)
-            await interaction.reply({
-                content: 'An error occurred while executing this command.',
-                ephemeral: true, //? Only visible to the user that called this.
-            })
+        if (interaction.isCommand()) {
+            return handleCommand(interaction)
+        } else if (interaction.isButton()) {
+            return handleButton(interaction)
         }
     }
 }
